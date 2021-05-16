cfa2c1a86c437fa0a8d895b4c425533547509195
saivishnu725 <saivishnu725@gmail.com>
2021-05-16 17:58:42 +0530

    Finished but won't recommend running it

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayer/audioplayer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Xylophone',
+      home: XyloPhone(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+String link =
+    'https://raw.githubusercontent.com/londonappbrewery/Xylophone-Flutter-Completed/master/assets';
+
+class XyloPhone extends StatefulWidget {
+  @override
+  _XyloPhoneState createState() => _XyloPhoneState();
+}
+
+class _XyloPhoneState extends State<XyloPhone> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      // appBar: AppBar(title: Text('XyloPhone')),
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            //          1
+            Expanded(
+                child: FlatButton(
+              onPressed: () => playSound('$link/note1.wav'),
+              color: Colors.red,
+              child: Container(),
+            )),
+            // Expanded(
+            //   child: TextButton(
+            //     onPressed: () => playSound('$link/note1.wav'),
+            //     child: Container(
+            //       color: Colors.red,
+            //     ),
+            //   ),
+            // ),
+            //          2
+            Expanded(
+              child: FlatButton(
+                color: Colors.orange,
+                onPressed: () => playSound('$link/note2.wav'),
+                child: Container(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            //          3
+            Expanded(
+              child: FlatButton(
+                color: Colors.yellow,
+                onPressed: () => playSound('$link/note3.wav'),
+                child: Container(),
+              ),
+            ),
+            //          4
+            Expanded(
+              child: FlatButton(
+                color: Colors.green,
+                onPressed: () => playSound('$link/note4.wav'),
+                child: Container(),
+              ),
+            ),
+            //          5
+            Expanded(
+              child: FlatButton(
+                color: Colors.teal,
+                onPressed: () => playSound('$link/note5.wav'),
+                child: Container(),
+              ),
+            ),
+            //        6
+            Expanded(
+              child: FlatButton(
+                color: Colors.blue,
+                onPressed: () => playSound('$link/note6.wav'),
+                child: Container(),
+              ),
+            ),
+            //        7
+            Expanded(
+              child: FlatButton(
+                color: Colors.purple,
+                onPressed: () => playSound('$link/note7.wav'),
+                child: Container(),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void playSound(String url) {
+  final audioPlayer = AudioPlayer();
+  audioPlayer.play(
+    url,
+  );
 }
